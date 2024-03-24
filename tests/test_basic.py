15c9438fca2f2302547aae6da048e82eea818f7f
Alexander Macdonald <alex@alexmac.cc>
2024-03-23 17:03:20 -0700

    CSP / Xray (#10)
    * Helpers for CSP/permissions policy headers + static file helper
    
    * AWS XRay support

--- tests/test_basic.py
+++ tests/test_basic.py
@@ -35,6 +35,44 @@ class Test(unittest.IsolatedAsyncioTestCase):
                     ) as resp:
                         assert resp.status == 200
                         assert (await resp.text()) == "{}"
+
+                        assert resp.headers == {
+                            "Content-Length": ANY,
+                            "content-security-policy": "default-src 'self'",
+                            "Content-Type": ANY,
+                            "Date": ANY,
+                            "referrer-policy": "strict-origin-when-cross-origin",
+                            "Server": ANY,
+                            "x-content-type-options": "nosniff",
+                            "x-frame-options": "SAMEORIGIN",
+                        }
+                s.shutdown_event.set()
+
+    async def test_custom_sec_headers(self):
+        s = ExampleServer()
+        async with asyncio.timeout(30):
+            async with asyncio.TaskGroup() as tg:
+                tg.create_task(s.run_app(log))
+                await asyncio.sleep(1)
+                async with aiohttp.ClientSession() as session:
+                    async with session.get(
+                        URL.build(
+                            host=s.host, port=s.port, path="/api/custom-sec-headers"
+                        )
+                    ) as resp:
+                        assert resp.status == 200
+                        assert (await resp.text()) == "{}"
+
+                        assert resp.headers == {
+                            "Content-Length": ANY,
+                            "content-security-policy": "foo",
+                            "Content-Type": ANY,
+                            "Date": ANY,
+                            "referrer-policy": "origin",
+                            "Server": ANY,
+                            "x-content-type-options": "bar",
+                            "x-frame-options": "waz",
+                        }
                 s.shutdown_event.set()
 
     async def test_bad_api(self):
@@ -51,6 +89,16 @@ class Test(unittest.IsolatedAsyncioTestCase):
                         assert (await resp.json()) == {
                             "error": "Unhandled Exception",
                             "request_id": ANY,
+                        }
+                        assert resp.headers == {
+                            "Content-Length": ANY,
+                            "content-security-policy": "default-src 'self'",
+                            "Content-Type": ANY,
+                            "Date": ANY,
+                            "referrer-policy": "strict-origin-when-cross-origin",
+                            "Server": ANY,
+                            "x-content-type-options": "nosniff",
+                            "x-frame-options": "SAMEORIGIN",
                         }
                 s.shutdown_event.set()
 
@@ -107,3 +155,17 @@ class Test(unittest.IsolatedAsyncioTestCase):
                     assert resp.status == 500
                     assert (await resp.text()).startswith("500 Internal Server Error")
             s.shutdown_event.set()
+
+    async def test_get_file(self):
+        s = ExampleServer()
+        async with asyncio.timeout(30):
+            async with asyncio.TaskGroup() as tg:
+                tg.create_task(s.run_app(log))
+                await asyncio.sleep(1)
+                async with aiohttp.ClientSession() as session:
+                    async with session.get(
+                        URL.build(host=s.host, port=s.port, path="/api/license")
+                    ) as resp:
+                        assert resp.status == 200
+                        assert (await resp.text()).startswith("MIT License")
+                s.shutdown_event.set()
