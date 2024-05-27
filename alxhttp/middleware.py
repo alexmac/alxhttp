fa2dbf2ee20325d291858619f44cb97f0c2dcd5c
Alexander Macdonald <alex@alexmac.cc>
2024-05-26 21:21:49 -0700

    Convert HTTP Errors to JSON (#13)
    * Ensure aiohttp errors are JSON
    
    * specifiy the package name explicitly
    
    * fix package def

--- alxhttp/middleware.py
+++ alxhttp/middleware.py
@@ -7,6 +7,7 @@ from aiohttp.typedefs import Handler, Middleware
 from aiohttp.web import HTTPException, Request, json_response, middleware
 from multidict import CIMultiDict
 from alxhttp.headers import content_security_policy
+from aiohttp.web_exceptions import HTTPClientError, HTTPServerError
 
 from alxhttp.req_id import get_request_id, set_request_id, current_request
 from alxhttp.xray import get_xray_middleware
@@ -75,8 +76,79 @@ async def assign_req_id(request: Request, handler: Handler):
     current_request.reset(token)
 
 
+error_types = frozenset(
+  [
+    'HTTPBadGateway',
+    'HTTPBadRequest',
+    'HTTPClientError',
+    'HTTPConflict',
+    'HTTPExpectationFailed',
+    'HTTPFailedDependency',
+    'HTTPForbidden',
+    'HTTPGatewayTimeout',
+    'HTTPGone',
+    'HTTPInsufficientStorage',
+    'HTTPInternalServerError',
+    'HTTPLengthRequired',
+    'HTTPMethodNotAllowed',
+    'HTTPMisdirectedRequest',
+    'HTTPNetworkAuthenticationRequired',
+    'HTTPNotAcceptable',
+    'HTTPNotExtended',
+    'HTTPNotFound',
+    'HTTPNotImplemented',
+    'HTTPPaymentRequired',
+    'HTTPPreconditionFailed',
+    'HTTPPreconditionRequired',
+    'HTTPProxyAuthenticationRequired',
+    'HTTPRequestEntityTooLarge',
+    'HTTPRequestHeaderFieldsTooLarge',
+    'HTTPRequestRangeNotSatisfiable',
+    'HTTPRequestTimeout',
+    'HTTPRequestURITooLong',
+    'HTTPServerError',
+    'HTTPServiceUnavailable',
+    'HTTPTooManyRequests',
+    'HTTPUnauthorized',
+    'HTTPUnavailableForLegalReasons',
+    'HTTPUnprocessableEntity',
+    'HTTPUnsupportedMediaType',
+    'HTTPUpgradeRequired',
+    'HTTPVariantAlsoNegotiates',
+    'HTTPVersionNotSupported',
+  ]
+)
+
+
+@middleware
+async def ensure_json_errors(request: Request, handler: Handler):
+  """
+  Ensure that any of AioHTTP's 4XX/5XX exceptions become JSON
+  responses. Note that this intentionally avoids messing around
+  with subclasses of these exceptions.
+  """
+  try:
+    return await handler(request)
+  except (HTTPClientError, HTTPServerError) as e:
+    if type(e).__name__ in error_types:
+      # It's one of the native errors, not a subclass
+      return json_response(
+        {
+          'error': e.reason,
+          'request_id': get_request_id(request),
+        },
+        status=e.status_code,
+      )
+    raise
+
+
 def default_middleware(include_xray: bool = False) -> List[Middleware]:
-  middlewares = [assign_req_id, default_security_headers, unhandled_error_handler]
+  middlewares = [
+    assign_req_id,
+    default_security_headers,
+    unhandled_error_handler,
+    ensure_json_errors,
+  ]
 
   xray_middleware = get_xray_middleware()
   if xray_middleware is not None and include_xray:
